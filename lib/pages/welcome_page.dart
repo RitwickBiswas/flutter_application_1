0b8d98adcda65dc1f905645abcd2707060d6bd9e
RitwickBiswas <ritwick.biswas95@gmail.com>
2022-08-20 13:51:45 +0530

    add large text widget

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_application_1/widgets/large_text.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -30,6 +31,16 @@ class _WelcomePageState extends State<WelcomePage> {
                   image: DecorationImage(
                       image: AssetImage("img/" + images[index]),
                       fit: BoxFit.cover),
+                ),
+                child: Container(
+                  margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+                  child: Row(
+                    children: [
+                      Column(
+                        children: [Largetext(text: "Trips")],
+                      )
+                    ],
+                  ),
                 ),
               );
             }));
